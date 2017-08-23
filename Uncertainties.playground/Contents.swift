4d3108965e6de81a9e6038bd3a9b6b051ee2adce
kirankunigiri <kirankunigiri@gmail.com>
2017-08-23 02:13:54 -0700

    Added Latex output capabilities
    Multiplication and division with uncertainty now produces Latex output
    which can be copied into Google Docs and converted into Latex equations
    using the Google Docs extension.

--- Uncertainties.playground/Contents.swift
+++ Uncertainties.playground/Contents.swift
@@ -3,6 +3,7 @@
 import Foundation
 
 
+// MARK: - Uncertain class
 
 /**
  Uncertain
@@ -33,42 +34,78 @@ class Uncertain: CustomStringConvertible {
     }
     
     class func multiply(_ a: Uncertain, _ b: Uncertain) -> Uncertain {
+        // Calculations
         let large = (a.num + a.certainty)*(b.num - b.certainty)
-        print("Largest result: \(large)")
-        print("Largest \\ result = \\frac{\(a.num!)+\(a.certainty!)}{\(b.num!)-\(b.certainty!)}")
         let small = (a.num - a.certainty)*(b.num + b.certainty)
-        print("Smallest result: \(small)")
-        print("Smallest \\ result = \\frac{\(a.num!)-\(a.certainty!)}{\(b.num!)+\(b.certainty!)}")
         let avgNum = (large + small)/2.0
-        print("Average result: \(avgNum)")
         let range = (large - small)
-        print("Range: \(range)")
         let result = Uncertain(num: avgNum, certainty: range/2.0)
+        
+        // Print output
+        print("Calculations")
+        print("Largest result: \(large)")
+        print("Smallest result: \(small)")
+        print("Average result: \(avgNum)")
+        print("Range: \(range)")
         print("Result: \(result)")
+        print("")
+        
+        // Print latex
+        print("LATEX Output")
+        print("$$Expression: (\(a.num!)\\pm\(a.certainty!))*(\(b.num!)\\pm\(b.certainty!))$$")
+        print("$$Largest \\ result = (\(a.num!)+\(a.certainty!))*(\(b.num!)-\(b.certainty!))=\(large)$$")
+        print("$$Smallest \\ result = (\(a.num!)-\(a.certainty!))*(\(b.num!)+\(b.certainty!))=\(small)$$")
+        print("$$Average \\ result = (\(large)+\(small))/(2)=\(avgNum)$$")
+        print("$$Range \\ result = \(large)-\(small)=\(range)$$")
+        print("$$Result: \(result)$$")
+        
+        // Return
         return Uncertain(num: avgNum, certainty: range/2.0)
     }
     
     class func divide(_ a: Uncertain, _ b: Uncertain) -> Uncertain {
         let large = (a.num + a.certainty)/(b.num - b.certainty)
-        print("Largest result: \(large)")
         let small = (a.num - a.certainty)/(b.num + b.certainty)
-        print("Smallest result: \(small)")
         let avgNum = (large + small)/2.0
-        print("Average result: \(avgNum)")
         let range = (large - small)
-        print("Range: \(range)")
         let result = Uncertain(num: avgNum, certainty: range/2.0)
+        
+        // Print output
+        print("Calculations")
+        print("Largest result: \(large)")
+        print("Smallest result: \(small)")
+        print("Average result: \(avgNum)")
+        print("Range: \(range)")
         print("Result: \(result)")
-        return result
+        print("")
+        
+        // Print latex
+        print("LATEX Output")
+        print("$$Expression: \\frac{\(a.num!)\\pm\(a.certainty!)}{\(b.num!)\\pm\(b.certainty!)}$$")
+        print("$$Largest \\ result = \\frac{\(a.num!)+\(a.certainty!)}{\(b.num!)-\(b.certainty!)}=\(large)$$")
+        print("$$Smallest \\ result = \\frac{\(a.num!)-\(a.certainty!)}{\(b.num!)+\(b.certainty!)}=\(small)$$")
+        print("$$Average \\ result = \\frac{\(large)+\(small)}{2}=\(avgNum)$$")
+        print("$$Range \\ result = \(large)-\(small)=\(range)$$")
+        print("$$Result: \(result)$$")
+        
+        // Return
+        return Uncertain(num: avgNum, certainty: range/2.0)
     }
     
+    // Override to string method
     public var description: String {
-        return "\(num!) +/- \(certainty!)"
+        return "\(num!) \\pm \(certainty!)"
     }
+    
+    // TODO: Override operators
+    
+    // TODO: Round results based on number of digits in number and uncertainty (highest number of digits) 
     
 }
 
 
+
+// MARK: - Tests
 let number1 = Uncertain(num: 1.53, certainty: 0.05)
 let number2 = Uncertain(num: 0.67, certainty: 0.02)
 
@@ -80,8 +117,12 @@ print("Subtraction")
 let resultSubtraction = Uncertain.subtract(number1, number2)
 print("")
 
-print("Multiplication")
+print("\nMultiplication\n")
 let resultMultiplication = Uncertain.multiply(number1, number2)
+print("")
+
+print("\nDivision\n")
+let resultDivision = Uncertain.divide(number1, number2)
 print("")
 
 
